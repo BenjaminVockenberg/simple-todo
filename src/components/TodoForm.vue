abe1c342627de21fdbe6c68af76d55ba43aa6a49
benjamin.vockenberg <benjamin.vockenberg@gmail.com>
2019-02-08 14:38:57 +0100

    successfully moved from bootstrap to bulma

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -3,62 +3,80 @@
     <div class="columns">
         <div class="column">
             <form>
-                <div class="form-group">
-                    <label>Todo
-                    <sup>
-                    <strong>*</strong>
-                    </sup>
-                </label>
-                <input
-                    :class="{ 'error' : item.title === '' }"
-                    v-model="item.title"
-                    class="form-control"
-                    placeholder="Title of you Todo"
-                    type="text"
-                    @focus="touched = true"
-                >
+                <div class="field">
+                    <label class="label">Todo
+                        <sup class="control">
+                            <strong>*</strong>
+                        </sup>
+                    </label>
+                    <div class="control">
+                        <input
+                        :class="{ 'error' : item.title === '' }"
+                        v-model="item.title"
+                        class="input"
+                        placeholder="Title of you Todo"
+                        type="text"
+                        @focus="touched = true"
+                        >
+                    </div>
+                    
             </div>
-            <div v-if="item.title === '' && touched" class="alert alert-info">
+            <div v-if="item.title === '' && touched" class="notification is-primary">
                 <p>
                     <strong>Info:</strong>This field is required!
                 </p>
             </div>
-            <div class="form-group">
-                <label>Priority</label>
-                <select v-model="item.priority" class="form-control" name="priority">
-                    <option>High</option>
-                    <option>Normal</option>
-                    <option>Low</option>
-                </select>
+            <div class="field">
+                <label class="label">Priority</label>
+                <div class="control">
+                    <div class="select">
+                        <select v-model="item.priority" name="priority">
+                            <option>High</option>
+                            <option>Normal</option>
+                            <option>Low</option>
+                        </select>
+                    </div>
+                    
+                </div>
             </div>
-            <div class="form-group">
-                <label>Description</label>
-                <textarea
+            <div class="field">
+                <label class="label">Description</label>
+                <div class="control">
+                    <textarea
                     v-model="item.todo"
-                    class="form-control"
+                    class="textarea"
                     placeholder="Description of you Todo"
                     name="todo"
                     rows="5"
                     ols="80"
-                />
+                    />
+                </div>
+                
             </div>
-            <div class="form-group">
-                <button
+            <div class="field">
+                <div class="control">
+                    <a class="button is-primary"
                     :disabled="item.title === ''"
-                    class="btn btn-primary"
                     type="button"
                     name="add"
                     @submit.prevent
                     @click="add()"
-                >add</button>&nbsp;
-                <button
-                    :disabled="item.title === ''"
-                    class="btn btn-secondary"
-                    type="button"
-                    name="cancle"
-                    @submit.prevent
-                    @click="cancle()"
-                >cancle</button>
+                    >
+                        add
+                    </a>&nbsp;
+                
+                
+                    <a
+                        :disabled="item.title === ''"
+                        class="button"
+                        type="button"
+                        name="cancle"
+                        @submit.prevent
+                        @click="cancle()"
+                    >
+                        cancle
+                    </a>
+                </div>
             </div>
         </form>
     </div>
@@ -67,45 +85,48 @@
     <div class="column">
         <!-- Component TodoTitle -->
         <TodoTitle msg="Hallo Leute"/>
-        <div v-if="entry.length < 1" class="alert alert-warning">
+        <div v-if="entry.length < 1" class="notification is-warning">
             <p>
                 <strong>Hinweis:</strong> Keine Daten vorhanden!
             </p>
         </div>
-        <ul class="list-group">
-            <li v-for="(element, index) in entry" :key="index" class="list-group-item">
-                <div class="row">
-                    <div class="col-md-8">
-                        <span
-                            :class="{ 'badge badge-danger' : element.priority === 'High', 'badge badge-success' : element.priority === 'Normal', 'badge badge-info' : element.priority === 'Low' }"
-                        >
-                            {{ element.priority }}
-                        </span>
-                        <h3 :class="{ 'done' : element.addDone }">
-                            <strong>{{ element.title }}</strong>
-                        </h3>
-                        <p>{{ element.todo }}</p>
-                    </div>
-                    <div class="col-md-4">
-                        <a
-                            class="btn btn-success"
-                            type="button"
-                            @click="element.addDone = !element.addDone"
-                        >
-                            {{ element.addDone ? 'undone' : 'done' }}
-                        </a>&nbsp;
-                        <button
-                            class="button"
-                            type="button"
-                            name="delete"
-                            @click="deleteItem(index)"
-                        >
-                            x
-                        </button>
-                    </div>
+
+        <div class="box" :class="{ 'done' : element.addDone }" v-for="(element, index) in entry" :key="index">
+            <article class="media">
+                <div class="media-left">
+                    <span class="tag is-info is-large" :class="{ 'tag is-danger is-large' : element.priority === 'High', 'tag is-success is-large' : element.priority === 'Normal', 'tag is-info is-large' : element.priority === 'Low' }">{{ element.priority }}</span>
                 </div>
-            </li>
-        </ul>
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>{{ element.title }}</strong> 
+                            <br>
+                            {{ element.todo }}
+                        </p>
+                    </div>
+                    <nav class="level">
+                        <div class="level-left">
+                            <p class="buttons">                                
+                                <a class="button is-success" @click="element.addDone = !element.addDone">
+                                    <span class="icon is-small">
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                    <span>{{ element.addDone ? 'undone' : 'done' }}</span>
+                                </a>
+                                <a class="button is-danger is-outlined" @click="deleteItem(index)">
+                                    <span>Delete</span>
+                                    <span class="icon is-small">
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </a>
+                            </p>
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        </div>
+
+        
         </div>
     </div>
 </template>
@@ -115,63 +136,63 @@
 import TodoTitle from "./TodoTitle.vue";
 
 export default {
-  name: "TodoForm",
+    name: "TodoForm",
 
-  components: {
-    TodoTitle
-  },
-
-  data() {
-    return {
-      entry: [],
-      touched: false,
-      // DataObj storing content of entry array
-      item: {
-        title: "",
-        priority: "Normal",
-        todo: "",
-        addDone: false
-      }
-    };
-  },
-  methods: {
-    /**
-     * @name add()
-     * @desc adding a new item to the entry array
-     */
-    add() {
-      this.entry.push(this.item);
-      this.item = { title: "", priority: "Normal", todo: "", addDone: false };
-      this.touched = false;
+    components: {
+        TodoTitle
     },
 
-    /**
-     * @name cancle()
-     * @desc clears all input fields
-     */
-    cancle() {
-      this.item = { title: "", priority: "Normal", todo: "", addDone: false };
-      this.touched = false;
+    data() {
+        return {
+            entry: [],
+            touched: false,
+            // DataObj storing content of entry array
+            item: {
+                title: "",
+                priority: "Normal",
+                todo: "",
+                addDone: false
+            }
+        };
     },
+    methods: {
+        /**
+        * @name add()
+        * @desc adding a new item to the entry array
+        */
+        add() {
+            this.entry.push(this.item);
+            this.item = { title: "", priority: "Normal", todo: "", addDone: false };
+            this.touched = false;
+        },
 
-    /**
-     * @name deleteItem(index)
-     * @param index { number }
-     * @desc deletes a specific item from the entries array
-     */
-    deleteItem(index) {
-      this.entry.splice(index, 1);
+        /**
+         * @name cancle()
+         * @desc clears all input fields
+         */
+        cancle() {
+            this.item = { title: "", priority: "Normal", todo: "", addDone: false };
+            this.touched = false;
+        },
+
+        /**
+         * @name deleteItem(index)
+         * @param index { number }
+         * @desc deletes a specific item from the entries array
+         */
+        deleteItem(index) {
+            this.entry.splice(index, 1);
+        }
     }
-  }
 };
 </script>
 
 <style scoped lang="scss">
 .done {
-  text-decoration: line-through;
+    opacity: 0.5;
 }
 
 .error {
-  border: 1px solid rgb(255, 0, 0);
+    border: 1px solid rgb(255, 0, 0);
 }
 </style>
